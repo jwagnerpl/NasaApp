ac889d147dd1ad4e5f0dc010c304f2bd233013ec
Joseph Wagner <joseph.j.wagner360@gmail.com>
2018-01-18 16:27:59 -0800

    Added earthview fields for address.

---
+++ app/src/main/java/teamtreehouse/com/nasaapp/ui/fragments/MapFragment.java
@@ -0,0 +1,21 @@
+package teamtreehouse.com.nasaapp.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends android.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
